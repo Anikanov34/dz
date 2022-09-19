c9b10c42975b082cd1e113b677e9ca227a9c4ea2
Anikanov <ivanov@mail.ru>
2022-09-19 20:41:30 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2)
+{
+   Console.WriteLine("Первое число больше второго"); 
+}
+else if (num1<num2)
+{
+     Console.WriteLine("Первое число меньше второго");   
+}
+else
+{
+   Console.WriteLine("Оба числа равны"); 
+}

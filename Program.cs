2b8cc00f3f225f4432eabd65c061338f6e1c272b
Anikanov <ivanov@mail.ru>
2022-09-19 21:17:36 +0300

    дз 2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 
-Console.WriteLine("введите первое число");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите второе число");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-if(num1>num2)
+Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1; 
+int ost = 0;
+while (i <= N)
 {
-   Console.WriteLine("Первое число больше второго"); 
-}
-else if (num1<num2)
+   ost = i % 2;
+   if (ost==0)
 {
-     Console.WriteLine("Первое число меньше второго");   
+   Console.Write($"{i} ,");
+   i++;
 }
 else
 {
-   Console.WriteLine("Оба числа равны"); 
+   i++;
+}
 }
